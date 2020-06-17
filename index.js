7784e78abf226553e81546c4071ad56932f3406d
KDH <temeddix@gmail.com>
2020-06-17 20:10:49 +0900

    헬로월드 시도 1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("Initial index file of Yomium. Hello World! 건덕동의 오픈웹툰 플랫폼 요미움 개발 프로젝트");
 });
 
 const port = process.env.PORT || 1337;
