38b1e7bfb9a5072f51694682647073ca5411dff2
JohnSilverman <52631852+JohnSilverman@users.noreply.github.com>
2021-02-03 12:05:06 +0900

    Update sendMail.js

--- backend/ajax/base/sendMail.js
+++ backend/ajax/base/sendMail.js
@@ -1,30 +1,25 @@
 const Nodemailer = require('nodemailer');
 function sendMail(email,htmltext){
-    try {
-        const mailConfig = {
-            host : "smtp.sendgrid.net",
-            port : 587,
-            auth : {
-                user : "apikey",
-                pass : "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"
-            },
-            requireTLS : true
-        };
+    const mailConfig = {
+        host : "smtp.sendgrid.net",
+        port : 587,
+        auth : {
+            user : "apikey",
+            pass : "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"
+        },
+        requireTLS : true
+    };
 
-        let message = {
-            from : "no-reply@zeminem.com",
-            to : email,
-            subject : "Verification Code",
-            html : htmltext
-        };
+    let message = {
+        from : "no-reply@zeminem.com",
+        to : email,
+        subject : "Verification Code",
+        html : htmltext
+    };
 
-        let transporter = Nodemailer.createTransport(mailConfig);
-        transporter.sendMail(message);
+    let transporter = Nodemailer.createTransport(mailConfig);
+    transporter.sendMail(message);
 
-    } catch(error){
-        console.log("Sendmail error",error);
-        return false;
-    }
 }
 
 module.exports = sendMail;
