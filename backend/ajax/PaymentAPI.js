bea72f9c61f5e5980660461213dfbf3d44c48a26
JohnSilverman <52631852+JohnSilverman@users.noreply.github.com>
2021-02-05 23:19:12 +0900

    Update PaymentAPI.js

--- backend/ajax/PaymentAPI.js
+++ backend/ajax/PaymentAPI.js
@@ -32,84 +32,143 @@ router.use("/ajax/payment",function(req,res,next){
 	}
 });
 
+//For debugging : process iamporter promise
+function processIamporterPromiseForDebugging(promise){
+	promise.then(result=>{
+		Base.logInfo("payment api result",result);
+		Base.resYes(res,"payment result",result);
+	})
+	.catch(err => {
+		if(err instanceof IamporterError){
+			Base.logErr("payment Iamporter Error",err);
+			Base.resNo(res,"payment module error");
+		} else {
+			Base.logErr("payment error",err);
+			Base.resNo(res,"server error");
+		}
+	});
+}
+
 //빌링키 생성
 router.post("/ajax/payment/subscription/create", function(req,res){
-
+	processIamporterPromiseForDebugging(
+		iamporter.createSubscription({
+			"customoer_uid" : req.body.customoer_uid,
+			"card_number" : req.body.card_number,//'1234-1234-1234-1234'
+			"expiry" : req.body.expiry,//'2021-11'
+			"birth" : req.body.birth,///'971002'
+			"pwd_2digit" : req.body.pwd_2digit//'99'
+		})
+	);
 });
 
 //빌링키 조회
 router.post("/ajax/payment/subscription/get",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		iamporter.getSubscription(req.body.customoer_uid)
+	);
 });
 
 //빌링키 삭제
 router.post("/ajax/payment/subscription/delete",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		iamporter.deleteSubscription(req.body.customoer_uid)
+	);
 });
 
 //빌링키 결제
 router.post("/ajax/payment/subscription/pay",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		iamporter.paySubscription({
+			"customoer_uid" : req.body.customoer_uid,
+			"merchant_uid" : req.body.merchant_uid,
+			"amount" : Number(req.body.amount)
+		})
+	);
 });
 
 //비인증 일회성 결제
 router.post("/ajax/payment/onetime/pay",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		
+	);
 });
 
 //해외카드 비인증 결제
 router.post("/ajax/payment/foreign/pay",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		
+		);
 });
 
 //결제 취소 : 아임포트 고유 아이디 : 설정된 계좌로 환불
 router.post("/ajax/payment/cancel/iamport/refund",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		
+		);
 });
 
 //결제 취소 : 아임포트 고유 아이디
 router.post("/ajax/payment/cancel/iamport",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		
+		);
 });
 
 //결제 취소 : 상점 고유 아이디 : 부분 취소
 router.post("/ajax/payment/cancel/merchant/part",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		
+		);
 });
 
 //결제 취소 : 상점 고유 아이디
 router.post("/ajax/payment/cancel/merchant",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		
+		);
 });
 
 //조회 : 아임포트 고유 아이디
 router.post("/ajax/payment/find/iamport",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		
+		);
 });
 
 //조회 : 상점 고유 아이디
 router.post("/ajax/payment/find/merchant",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		
+		);
 });
 
 //조회 : 결제 상태
 router.post("/ajax/payment/findall/status",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		
+		);
 });
 
 //조회 : 예약된 결제건
 router.post("/ajax/payment/find/prepared",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		
+		);
 });
 
 //예약
 router.post("/ajax/payment/prepare",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		
+		);
 });
 
 //가상 계좌 발급
 router.post("/ajax/payment/vbank",function(req,res){
-
+	processIamporterPromiseForDebugging(
+		
+		);
 });
 
 
